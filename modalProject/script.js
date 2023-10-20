00413da9c7e2414eb0af7012f8bf6d95d72b16b5
Riteshkrm~ <rtskrm@gmail.com>
2023-10-20 17:56:57 +0530

    first commit

---
+++ modalProject/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+}
+
+btnCloseModal.addEventListener('click', () => {
+    modal.classList.add('hidden');
+});
+
+btnCloseModal.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (e) {
+    
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+});
